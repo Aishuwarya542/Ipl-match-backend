4258c4fbd892d0cbd63727bc67c90c345760c482
Aishuwarya542 <aishuwarya.ramesh@gmail.com>
2024-09-20 11:44:04 +0530

    changes in swagger config

--- ipl-match/src/main/java/com/indium/ipl_match/config/SwaggerConfiguration.java
+++ ipl-match/src/main/java/com/indium/ipl_match/config/SwaggerConfiguration.java
@@ -1,22 +1,3 @@
-//package com.indium.ipl_match.config;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfiguration {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .title("IPL Match API")
-//                        .version("1.0")
-//                        .description("API for data management and statistics for IPL match"));
-//    }
-//}
 package com.indium.ipl_match.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
